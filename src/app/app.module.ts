34fad255302118a7cef805802760dbb616b701ba
rrspencer <rrspencer1996@outlook.com>
2022-12-05 18:21:52 -0500

    content update proj 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { DivComponent } from './div/div.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideNavComponent,
+    BottomNavComponent,
+    FooterComponent,
+    DivComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/div/div.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-div',
+  templateUrl: './div.component.html',
+  styleUrls: ['./div.component.css']
+})
+export class DivComponent {
+
+}
